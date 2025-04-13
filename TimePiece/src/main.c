cd466e4291d248f345fd7896b63ecc180b1ac438
Julius de Boer <45075461+JuliusDeBoer@users.noreply.github.com>
2025-04-13 22:15:08 +0200

    feat: Add *some* time code

--- TimePiece/src/main.c
+++ TimePiece/src/main.c
@@ -1,8 +1,56 @@
 #include "sdcc.h"
 
+typedef struct {
+  // NOTE(Julius): God I with there were smaller ints
+  unsigned int hour;
+  unsigned int minute;
+  unsigned int second;
+} WatchData;
+
+volatile WatchData WATCH_DATA = {
+    .hour = 0,
+    .minute = 0,
+    .second = 0,
+};
+
+/**
+ * This function is tasked with the very important job of advancing time. Good
+ * to know that this heavy task is handled in C. :)
+ *
+ * The function modifies the global WATCH_DATA structure directly.
+ */
+inline void advanceTime(void) {
+  WATCH_DATA.second++;
+  if (WATCH_DATA.second < 60) {
+    return;
+  }
+
+  WATCH_DATA.second = 0;
+  WATCH_DATA.minute++;
+
+  if (WATCH_DATA.minute < 60) {
+    return;
+  }
+
+  WATCH_DATA.minute = 0;
+  WATCH_DATA.hour++;
+
+  if (WATCH_DATA.hour < 24) {
+    return;
+  }
+
+  WATCH_DATA.hour = 0;
+}
+
+inline void updateDisplay(void) {
+    // TODO(Julius)
+}
+
 void tim2Compare(void) __interrupt(14 /* TIM2 capture/compare */) {
   __asm__("; Cool inline asm!"
           "nop");
+  advanceTime();
+  updateDisplay();
 }
 
 void main(void) {}
--- TimePiece/src/sdcc.h
+++ TimePiece/src/sdcc.h
@@ -3,9 +3,9 @@
 
 // Suppress clangd errors
 #ifdef CLANGD
-#define __asm__() (void)0
-#define __critical()
-#define __interrupt()
+#define NULL #define __asm__(_)(void) 0
+#define __critical(_)
+#define __interrupt(_)
 #define __trap
 #endif // CLANGD
 #endif // SDCC_H
